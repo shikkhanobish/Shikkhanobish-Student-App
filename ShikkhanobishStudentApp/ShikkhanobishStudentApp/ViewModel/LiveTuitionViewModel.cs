9dc3b92ca84e55f0e3932fc3f80bcdef1d47cae7
Abid Hassan <abidhassandiba@gmail.com>
2021-11-14 17:31:10 +0600

    LiveTuiton api call

--- ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/LiveTuitionViewModel.cs
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/LiveTuitionViewModel.cs
@@ -20,16 +20,36 @@ namespace ShikkhanobishStudentApp.ViewModel
         public async Task GetLogList()
         {
             var lList = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getTuiTionLogNeW".GetJsonAsync<List<TuiTionLog>>();
+            var tList = await "https://api.shikkhanobish.com/api/ShikkhanobishTeacher/getAllTeacher".PostJsonAsync(new { }).ReceiveJson<List<Teacher>>();
+            
             List<TuiTionLog> ntll = new List<TuiTionLog>();
             foreach (var item in lList)
             {
+                
                 if (item.studentID == StaticPageToPassData.thisStudentInfo.studentID && item.tuitionLogStatus == 1)
                 {
                     ntll.Add(item);
                 }
             }
+            liveTuitionList = ntll;
 
-            //liveTuitionList = ntll;
+            TuiTionLog tuiTionLog = new TuiTionLog();
+            foreach (var item2 in tList)
+            {
+                if (item2.activeStatus == 1)
+                {
+                    tuiTionLog.teacherName = item2.name;
+                    tuiTionLog.isPendingTeacherAvailable = true;
+                    
+                }
+                else
+                {
+                    tuiTionLog.teacherName = "--";
+                    tuiTionLog.isPendingTeacherAvailable = false;
+                }
+            }
+            liveTuitionList.Add(tuiTionLog);
+
         }
         private void Performgobakc()
         {
