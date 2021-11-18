7d3139187ab2cd34ac839edeceb5b62e88cea8c4
Abid Hassan <abidhassandiba@gmail.com>
2021-11-18 17:59:12 +0600

    Upto popUpTagList

---
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/Model/UserTimelineTag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShikkhanobishStudentApp.Model
+{
+    public class UserTimelineTag
+    {
+        public int userID { get; set; }
+        public int tagID { get; set; }
+    }
+}
--- ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/QuizTimelineViewModel.cs
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/QuizTimelineViewModel.cs
@@ -13,6 +13,9 @@ namespace ShikkhanobishStudentApp.ViewModel
 {
     public class QuizTimelineViewModel : BaseViewModel, INotifyPropertyChanged
     {
+        List<UserTimelineTag> userTmTg = new List<UserTimelineTag>();
+        List<Post> plist = new List<Post>();
+        List<Tag> tlist = new List<Tag>();
         public QuizTimelineViewModel()
         {
             //List<int> a = new List<int>();
@@ -30,22 +33,115 @@ namespace ShikkhanobishStudentApp.ViewModel
         #region Methods
         public async Task GetPostList()
         {
-            var plist = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getPost".GetJsonAsync<List<Post>>();
-            var tlist = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getTag".GetJsonAsync<List<Tag>>();
+            userTmTg = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getUserTimelineTagWithUserID".PostJsonAsync(new { userID = 10000152 }).ReceiveJson<List<UserTimelineTag>>();
+            plist = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getPost".GetJsonAsync<List<Post>>();
+            tlist = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getTag".GetJsonAsync<List<Tag>>();
+
+            
 
             List<Post> updatedPostList = new List<Post>();
+            
             foreach (var post in plist)
             {
                 foreach(var tag in tlist)
                 {
-                    if ( post.userID== 10000152 && post.tagID==tag.tagID)
+                    foreach(var utt in userTmTg)
                     {
-                        post.tagName = tag.tagName;
-                        updatedPostList.Add(post);
+                        if (post.tagID == utt.tagID)
+                        {
+                            post.tagName = tag.tagName;
+                            updatedPostList.Add(post);
+                        }
                     }
+                    
                 }
             }
             postList = updatedPostList;
+            await GetTagChip();
+
+
+            List<Tag> popUpTagList = new List<Tag>();
+            
+            foreach (var item in tlist)
+            {
+                popUpTagList.Add(item);
+            }
+            popUptagList = popUpTagList;
+        }
+        public async Task GetTagChip()
+        {
+            List<TagChip> thisTagChipList = new List<TagChip>();
+            int listCount = 0;
+            TagChip tc = new TagChip();
+            bool firstTime = true;
+            List<string> tagname = new List<string>();
+            foreach (var tl in userTmTg)
+            {
+                foreach(var tname in tlist)
+                {
+                    if(tl.tagID == tname.tagID)
+                    {
+                        tagname.Add(tname.tagName);
+                    }
+                }
+
+
+            }
+            
+            for(int i = 0; i < tagname.Count+1; i++)
+            {
+                if (listCount % 3 == 0)
+                {
+                    if (!firstTime)
+                    {
+                        thisTagChipList.Add(tc);
+                    }
+                    firstTime = false;
+                    tc = new TagChip();
+                   
+                    if (i != tagname.Count)
+                    {
+                        tc.tag1 = tagname[i];
+                        string bc1 = ChooseRandomColor();
+                        string bc2 = ChooseRandomColor();
+                        string bc3 = ChooseRandomColor();
+                        tc.backColor1 = "#10" + bc1;
+                        tc.backColor2 = "#10" + bc2;
+                        tc.backColor3 = "#10" + bc3;
+                        tc.backColortxt1 = "#" + bc1;
+                        tc.backColortxt2 = "#" + bc2;
+                        tc.backColortxt3 = "#" + bc3;
+                    }
+                    
+                }
+                else if (listCount % 3 == 1)
+                {
+                    tc.tag2 = tagname[i];
+                }
+                else if (listCount % 3 == 2)
+                {
+                    tc.tag3 = tagname[i];
+                }
+                listCount++;
+            }
+            tagList = thisTagChipList;
+
+        }
+
+        public void TagBackColor() {
+
+            TagChip tc = new TagChip();
+            Random rnd = new Random();
+            string bc1 = ChooseRandomColor();
+            string bc2 = ChooseRandomColor();
+            string bc3 = ChooseRandomColor();
+            tc.backColor1= "#10"+bc1;
+            tc.backColor2 = "#10"+ bc2;
+            tc.backColor3 = "#10"+bc3;
+            tc.backColortxt1 = "#"+bc1;
+            tc.backColortxt2 = "#" + bc2;
+            tc.backColortxt3 = "#" + bc3;
+
         }
         public ICommand answerQuestion
         {
@@ -57,6 +153,34 @@ namespace ShikkhanobishStudentApp.ViewModel
                 });
             }
         }
+        public string ChooseRandomColor()
+        {
+            List<string> colorName = new List<string>();
+            colorName.Add("7012B0");
+            colorName.Add("B01E12");
+            colorName.Add("58B012");
+            colorName.Add("12ACB0");
+            colorName.Add("1252B0");
+            colorName.Add("7212B0");
+            colorName.Add("B012A8");
+            colorName.Add("B01252");
+
+            colorName.Add("1287B1");
+            colorName.Add("0D8275");
+            colorName.Add("820D60");
+            colorName.Add("0D8244");
+            colorName.Add("6C16A3");
+            colorName.Add("B92815");
+            colorName.Add("51B915");
+            colorName.Add("D91843");
+            Random rnd = new Random();
+            int index = rnd.Next(0,15);
+
+            return colorName[index];
+
+
+
+        }
         #endregion
 
         #region Bindings
@@ -64,6 +188,16 @@ namespace ShikkhanobishStudentApp.ViewModel
         private List<Post> postList1;
 
         public List<Post> postList { get => postList1; set => SetProperty(ref postList1, value); }
+
+        private List<TagChip> tagList1;
+
+        public List<TagChip> tagList { get => tagList1; set => SetProperty(ref tagList1, value); }
+
+        private List<Tag> popUptagList1;
+
+        public List<Tag> popUptagList { get => popUptagList1; set => SetProperty(ref popUptagList1, value); }
         #endregion
     }
+
+    
 }
