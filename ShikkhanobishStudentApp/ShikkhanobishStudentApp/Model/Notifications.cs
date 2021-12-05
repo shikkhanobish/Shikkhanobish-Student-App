38bee423bfe5a2a0eefdd88a642c40901a6e7480
Abid Hassan <abidhassandiba@gmail.com>
2021-12-05 17:29:55 +0600

    Upto Notification from server

---
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/Model/Notifications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShikkhanobishStudentApp.Model
+{
+    public class Notifications
+    {
+        public string notificationID { get; set; }
+        public int userId { get; set; }
+        public int userType { get; set; }
+        public int notificationType { get; set; }
+        public string description { get; set; }
+        public string refIDOne { get; set; }
+        public string refIDTwo { get; set; }
+        public string refIDThree { get; set; }
+        public string refIDFour { get; set; }
+        public string notificationDate { get; set; }
+        public string titleColor { get; set; }
+        public string spanOne { get; set; }
+        public string spanTwo { get; set; }
+        public string spanThree { get; set; }
+    }
+}
---
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/NotificationViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Flurl.Http;
+using ShikkhanobishStudentApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishStudentApp.ViewModel
+{
+    public class NotificationViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        List<Notifications> nList = new List<Notifications>();
+        public NotificationViewModel()
+        {
+            GetNotification();
+        }
+
+        #region Methods
+        public async Task GetNotification()
+        {
+
+            nList = await "https://api.shikkhanobish.com/api/ShikkhanobishLogin/getNotification".GetJsonAsync<List<Notifications>>();
+            List<Notifications> updatedList = new List<Notifications>();
+            foreach (var item in nList)
+            {
+                if (item.notificationType==1)
+                {
+                    item.titleColor = "#0D94D5";
+                }
+                if (item.notificationType == 2)
+                {
+                    item.titleColor = "#A20DD5";
+                }
+                if (item.notificationType == 3)
+                {
+                    item.titleColor = "#0D6AD5";
+                }
+                if (item.notificationType == 4)
+                {
+                    item.titleColor = "#D5800D";
+                }
+                updatedList.Add(item);
+            }
+            notificationList = updatedList;
+
+        }
+        #endregion
+
+        #region Bindings
+        private List<Notifications> notificationList1;
+
+        public List<Notifications> notificationList { get => notificationList1; set => SetProperty(ref notificationList1, value); }
+
+        #endregion
+
+
+    }
+}
